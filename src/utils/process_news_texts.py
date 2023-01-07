d7eb496d811ba268293fe1d46369f6d97150f053
Chrome1278 <chromechromovich@gmail.com>
2023-01-07 19:20:13 +0300

    Исправление ошибки в обработке текстов

--- src/utils/process_news_texts.py
+++ src/utils/process_news_texts.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 texts_df = pd.read_csv('./data/raw/lenta.csv')
-texts_df = texts_df[texts_df['card-full-news__title'].str.len() > 10]
+texts_df = texts_df[texts_df['card-full-news__title'].str.len() > 5]
 texts_df = texts_df.drop_duplicates(subset=['card-full-news__title'])
 
-texts_df['card-full-news__title'].to_csv(
-    r'./data/processed/header_news.txt', header=None, index=None, sep=' ', mode='a'
+texts_df[['card-full-news__title']].to_csv(
+    r'./data/processed/header_news.txt', header=None, index=None, sep=' ', mode='w'
 )
-
-texts_df['card-full-news__title'][:10].to_csv(
-    r'./data/processed/header_news_small.txt', header=None, index=None, sep=' ', mode='a'
+texts_df[['card-full-news__title']][:10].to_csv(
+    r'./data/processed/header_news_small.txt', header=None, index=None, sep=' ', mode='w'
 )
 
 print(texts_df.shape)

c0089b9e74094baf67a316d140e73012a6a31eac
Chrome1278 <chromechromovich@gmail.com>
2023-01-10 23:33:02 +0300

    Создание функционала для обработки набора текстов

--- src/models/spacy_ner_model.py
+++ src/models/spacy_ner_model.py
@@ -5,22 +5,23 @@ import pandas as pd
 
 class SpacyModel:
     def __init__(self):
-        new_vocab = ['PRO']
         self.nlp = spacy.load('ru_core_news_md')
-        print(self.nlp.vocab.strings)
-        for item in new_vocab:
-            self.nlp.vocab.strings.add(item)
-        extended_nlp = spacy.load('./src/models/tuning_models_news/model-best')
-        print(extended_nlp.get_pipe('ner').labels)
-        print(self.nlp.get_pipe("ner").labels)
-        self.nlp.add_pipe(
-            'ner',
-            name="ner_extended",
-            source=extended_nlp,
-            after="ner",
-        )
-        print(self.nlp.get_pipe("ner").labels)
 
+        # new_vocab = ['PRO']
+        # self.nlp = spacy.load('ru_core_news_md')
+        # print(self.nlp.vocab.strings)
+        # for item in new_vocab:
+        #     self.nlp.vocab.strings.add(item)
+        # extended_nlp = spacy.load('./src/models/tuning_models_news/model-best')
+        # print(extended_nlp.get_pipe('ner').labels)
+        # print(self.nlp.get_pipe("ner").labels)
+        # self.nlp.add_pipe(
+        #     'ner',
+        #     name="ner_extended",
+        #     source=extended_nlp,
+        #     after="ner",
+        # )
+        # print(self.nlp.get_pipe("ner").labels)
         # self.extended_nlp = spacy.load('./src/models/tuning_models/model-best')
         # self.nlp = spacy.load('ru_core_news_md')
         # self.nlp.replace_listeners("tok2vec", "ner", ["model.tok2vec"])
@@ -40,7 +41,7 @@ class SpacyModel:
 
     def get_visualized_output(self, text: str):
         doc = self.nlp(text)
-        ent_svg = displacy.render(doc, style='ent', minify=True, jupyter=False)
+        ent_svg = displacy.render(doc, style='ent', minify=True, jupyter=False, page=True)
         return ent_svg
 
     def get_entities_df(self, text: str) -> pd.DataFrame:
--- src/web/frontend.py
+++ src/web/frontend.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
+
 from src.models.spacy_ner_model import SpacyModel
+from src.utils.work_with_df import DataFrameHandler
 
 
 class App:
@@ -32,27 +34,37 @@ class App:
                     st.dataframe(entities_df)
                     st.download_button(
                         label="Скачать таблицу с сущностями",
-                        data=self.df_to_excel(entities_df),
+                        data=DataFrameHandler.convert_df_to_excel(entities_df),
                         file_name='entities_df.xlsx',
                     )
                     st.markdown(' ')
                 else:
                     st.warning('В введённом тексте не найдено каких-либо сущностей!')
 
-    @staticmethod
-    def df_to_excel(df):
-        output = BytesIO()
-        writer = pd.ExcelWriter(output, engine='xlsxwriter')
-        df.to_excel(writer, index=False, sheet_name='Sheet1')
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-        format1 = workbook.add_format({'num_format': '0.00'})
-        worksheet.set_column('A:A', None, format1)
-        writer.save()
-        processed_data = output.getvalue()
-        return processed_data
+    def _analyze_dataset(self):
+        st.markdown(' ')
+        st.subheader('**Загрузите файл с текстами в поле ниже**')
+        uploaded_dataset = st.file_uploader(label='', type=['csv', 'txt'])
+        if uploaded_dataset is not None:
+            news_df = pd.read_csv(uploaded_dataset)
+            st.markdown(' ')
+            st.write(f"Количество текстов, загруженных для анализа: **{news_df.shape[0]}**")
+            st.markdown(' ')
+
+            total_df = pd.DataFrame()
+            for idx, text_to_analyze in news_df.iloc[:, :1].itertuples():
+                output_entities = self.nlp_model.get_output_entities(text_to_analyze)
+                if output_entities:
+                    entities_df = self.nlp_model.get_entities_df(text_to_analyze)
+                    entities_df['text_id'] = idx
+                    total_df = pd.concat([total_df, entities_df])
+
+            st.write(f"Количество найденных сущностей: **{total_df.shape[0]}**")
+            st.write(f"Количество уникальных сущностей: **{total_df.text.nunique()}**")
+            st.write(total_df)
 
     def __call__(self):
+        st.session_state.lang = 'ru'
         st.set_page_config(
             page_title="News NER",
             page_icon=":globe_with_meridians:",
@@ -74,3 +86,4 @@ class App:
             self._analyze_single_text()
         with many_texts_block:
             st.info('Находится в разработке!')
+            self._analyze_dataset()

7bda32ca8278abe0a01867b88af0866622959d7f
Chrome1278 <chromechromovich@gmail.com>
2023-05-16 12:57:45 +0300

    Обновление файлов по модели NER

--- src/utils/train_ner_model.py
+++ src/utils/train_ner_model.py
@@ -3,6 +3,7 @@ import time
 from pathlib import Path
 from spacy.cli.init_config import fill_config
 from spacy.cli.train import train
+import spacy_transformers
 
 # download('ru_core_news_md')
 
@@ -12,10 +13,14 @@ print('Initialization is complete!\n')
 start = time.time()
 
 train(
+
     Path("./src/configs/config.cfg"),
-    Path("./src/models/model_ner"),  # trained model
-    overrides={"paths.train": "./data/annotations/labelled_annotations.spacy"},
+    Path("./src/models/model_ner_large_5k"),  # trained model
+    # overrides={"paths.train": "./data/annotations/labelled_annotations.spacy"},
+    # use_gpu=0
 )
 
 end = time.time() - start
 print(f"\nLearning time: {end}")
+
+# best ~ 0.8
